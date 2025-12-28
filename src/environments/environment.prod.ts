d840624643b26b3838621c1622ad1d9989545e1f
Narayan Sahu <128928379+narayan439@users.noreply.github.com>
2025-12-28 21:21:28 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'homy-sofa-backend-production.up.railway.app',
+  apiBaseUrl: 'https://homy-sofa-backend-production.up.railway.app',
   apiPrefix: '/api'
 };
